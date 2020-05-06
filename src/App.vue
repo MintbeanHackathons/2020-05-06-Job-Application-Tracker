e0f491cdcf0ac7516ee59f65dcda2435aeddcb06
Derek Johnston <Derek@dj-design.ca>
2020-05-06 15:00:21 -0400

    job appliaction tracker

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    AppTracker
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTracker from './components/AppTracker.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppTracker
   }
 }
 </script>
@@ -23,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
